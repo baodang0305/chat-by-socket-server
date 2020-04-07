d7ee3379412f37b0f5a46d0a4d9dfcf8d14c658f
Bao Dang <hoaibaodang1997@gmail.com>
2020-04-07 18:13:56 +0700

    make it better

--- chatBySocket.js
+++ chatBySocket.js
@@ -24,7 +24,7 @@ const chatBySocket = (io) => {
 
             await addUser(user);
 
-            users.map(userItem => {
+            users.map(async (userItem) => {
                 const usersActive = await getUsersActive({"mEmail": userItem.mEmail, "fID": user.Item.fID});
                 io.to(userItem.mSocketID).emit("server-send-list-user-active", usersActive);
             });
